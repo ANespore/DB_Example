23ebb006809c2b4a9415609f243b32efb223efde
ligaband <90417731+ligaband@users.noreply.github.com>
2021-10-23 19:21:55 +0300

    Update MovieDAO.java
    added methods: add record; delete record by id; update record by id; search by id; download all records

--- src/main/java/Products/MovieDAO.java
+++ src/main/java/Products/MovieDAO.java
@@ -36,4 +36,60 @@ public class MovieDAO {
 
 
     }
+    
+    public String createMovie(Movie movie){
+        try{
+            movieRepository.create(movie);
+            return "Movie created successfully.";
+        }catch (SQLException e){
+            e.printStackTrace();
+            return "Error creating movie!";
+        }
+
+    }
+
+    public void updateMovie(Movie movie){
+        try{
+            movieRepository.update(movie);
+            System.out.println("Movie updated successfully");
+
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+
+    }
+
+
+    public Movie findMovie(int id){
+
+        try{
+            return movieRepository.findMovie(id);
+
+        }catch (SQLException e){
+            System.out.println("Cannot find movie with id:" + id);
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+        public void deleteMovie(int id){
+        try{
+            movieRepository.delete(id);
+            System.out.println("Movie deleted successfully.");
+
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+    }
+
+        public ArrayList<Movie> getAllMovies(){
+        try{
+            return movieRepository.getAll();
+        }catch(SQLException e){
+        e.printStackTrace();
+        return null;
+        }
+    }
+
 }
